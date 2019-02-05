e618148341c63182bb3d6186cd7f0a9d5420e468
Miguel Vieira <jmvieira@gmail.com>
2019-02-05 11:17:09 +0000

    Autopep8 and added install requires wagtail>2.4

--- setup.py
+++ setup.py
@@ -14,20 +14,20 @@ def get_version(*file_paths):
     """Retrieves the version from kdl_wagtail/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+    version_match = re.search(r'^__version__ = ['\']([^'\"]*)['\"]",
                               version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("kdl_wagtail", "__init__.py")
+version = get_version('kdl_wagtail', '__init__.py')
 
 
 if sys.argv[-1] == 'publish':
     try:
         import wheel
-        print("Wheel version: ", wheel.__version__)
+        print('Wheel version: ', wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
@@ -37,9 +37,9 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
+    print('Tagging the version on git:')
+    os.system('git tag -a %s -m 'version %s'' % (version, version))
+    os.system('git push --tags')
     sys.exit()
 
 readme = open('README.rst').read()
@@ -57,8 +57,8 @@ setup(
         'kdl_wagtail',
     ],
     include_package_data=True,
-    install_requires=[],
-    license="MIT",
+    install_requires=['wagtail>2.4'],
+    license='MIT',
     zip_safe=False,
     keywords='django-kdl-wagtail',
     classifiers=[
