5fa9a1649f74a5ecc6697faa1d6056b0343e1915
Miguel Vieira <jmvieira@pm.me>
2020-10-16 17:30:33 +0100

    🐛 Fix: Call to invalid function.

--- kdl_wagtail/people/models.py
+++ kdl_wagtail/people/models.py
@@ -34,47 +34,58 @@ class BasePerson(index.Indexed, ClusterableModel):
     the relationships to the database.
     https://github.com/wagtail/django-modelcluster
     """
-    _title = models.CharField('Title', max_length=254, blank=True, null=True)
-    name = models.CharField('Name', max_length=254)
+
+    _title = models.CharField("Title", max_length=254, blank=True, null=True)
+    name = models.CharField("Name", max_length=254)
 
     introduction = models.TextField(
-        blank=True, help_text='Short summary about the person')
-    image = models.ForeignKey('wagtailimages.Image', null=True,
-                              blank=True, on_delete=models.SET_NULL,
-                              related_name='+')
+        blank=True, help_text="Short summary about the person"
+    )
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
 
-    description = RichTextField(blank=True, help_text='Person bio/description')
+    description = RichTextField(blank=True, help_text="Person bio/description")
 
     api_fields = [
-        APIField('_title'),
-        APIField('name'),
-        APIField('introduction'),
-        APIField('image'),
-        APIField('description')
+        APIField("_title"),
+        APIField("name"),
+        APIField("introduction"),
+        APIField("image"),
+        APIField("description"),
     ]
 
     panels = [
-        MultiFieldPanel([
-            FieldRowPanel([
-                FieldPanel('_title', classname='col6'),
-                FieldPanel('name', classname='col6'),
-            ])
-        ], 'Name'),
-        FieldPanel('introduction', 'full'),
-        ImageChooserPanel('image'),
-        FieldPanel('description', 'full')
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("_title", classname="col6"),
+                        FieldPanel("name", classname="col6"),
+                    ]
+                )
+            ],
+            "Name",
+        ),
+        FieldPanel("introduction", "full"),
+        ImageChooserPanel("image"),
+        FieldPanel("description", "full"),
     ]
 
     search_fields = [
-        index.SearchField('name'),
-        index.SearchField('introduction'),
-        index.SearchField('description')
+        index.SearchField("name"),
+        index.SearchField("introduction"),
+        index.SearchField("description"),
     ]
 
     class Meta:
         abstract = True
-        verbose_name = 'Person'
-        verbose_name_plural = 'People'
+        verbose_name = "Person"
+        verbose_name_plural = "People"
 
     def __str__(self):
         return self.title
@@ -82,14 +93,13 @@ class BasePerson(index.Indexed, ClusterableModel):
     @property
     def thumbnail(self):
         if self.image:
-            return self.image.get_rendition('fill-50x50').img_tag()
+            return self.image.get_rendition("fill-50x50").img_tag()
 
         return None
 
     @property
     def title(self):
-        return '{} {}'.format(
-            self._title if self._title else '', self.name).strip()
+        return "{} {}".format(self._title if self._title else "", self.name).strip()
 
 
 @register_snippet
@@ -103,20 +113,19 @@ def get_person_model():
     `kdl_wagtail_people.Person`.
     """
     try:
-        return apps.get_model(
-            settings.KDL_WAGTAIL_PERSON_MODEL, require_ready=False)
+        return apps.get_model(settings.KDL_WAGTAIL_PERSON_MODEL, require_ready=False)
     except AttributeError:
         return Person
     except ValueError:
         raise ImproperlyConfigured(
-            ('KDL_WAGTAIL_PERSON_MODEL must be of the form '
-                '"app_label.model_name"')
+            ("KDL_WAGTAIL_PERSON_MODEL must be of the form " '"app_label.model_name"')
         )
     except LookupError:
         raise ImproperlyConfigured(
-            ('KDL_WAGTAIL_PERSON_MODEL refers to model "{}" '
-                'that has not been installed'.format(
-                    settings.KDL_WAGTAIL_PERSON_MODEL))
+            (
+                'KDL_WAGTAIL_PERSON_MODEL refers to model "{}" '
+                "that has not been installed".format(settings.KDL_WAGTAIL_PERSON_MODEL)
+            )
         )
 
     return Person
@@ -131,58 +140,56 @@ class PeopleIndexPersonRelationship(Orderable, models.Model):
     `PeopleIndexPage` below. This allows Persons to be added to a
     PeopleIndexPage.
     """
+
     page = ParentalKey(
-        'PeopleIndexPage', related_name='peopleindex_person_relationship',
-        on_delete=models.CASCADE
+        "PeopleIndexPage",
+        related_name="peopleindex_person_relationship",
+        on_delete=models.CASCADE,
     )
     person = models.ForeignKey(
-        PersonModel, related_name='person_peopleindex_relationship',
-        on_delete=models.CASCADE
+        PersonModel,
+        related_name="person_peopleindex_relationship",
+        on_delete=models.CASCADE,
     )
-    panels = [
-        SnippetChooserPanel('person')
-    ]
+    panels = [SnippetChooserPanel("person")]
 
 
 class PeopleIndexPage(BaseIndexPage):
     api_fields = BaseIndexPage.api_fields + [
-        APIField('peopleindex_person_relationship')
+        APIField("peopleindex_person_relationship")
     ]
 
     content_panels = BaseIndexPage.content_panels + [
-        InlinePanel('peopleindex_person_relationship',
-                    label='People', panels=None, min_num=1)
+        InlinePanel(
+            "peopleindex_person_relationship", label="People", panels=None, min_num=1
+        )
     ]
 
     search_fields = BaseIndexPage.search_fields + [
-        index.RelatedFields('peopleindex_person_relationship', [
-            index.SearchField('person')
-        ])
+        index.RelatedFields(
+            "peopleindex_person_relationship", [index.SearchField("person")]
+        )
     ]
 
     def people(self):
-        return self.peopleindex_person_relationship.live().select_related(
-            'person')
+        return self.peopleindex_person_relationship.all().select_related("person")
 
     def get_context(self, request):
         context = super().get_context(request)
-        context['people'] = self.people()
+        context["people"] = self.people()
 
         return context
 
 
 class PersonPage(BasePage):
     person = models.ForeignKey(
-        PersonModel, related_name='pages', on_delete=models.PROTECT)
+        PersonModel, related_name="pages", on_delete=models.PROTECT
+    )
 
-    api_fields = BasePage.api_fields + [
-        APIField('person')
-    ]
+    api_fields = BasePage.api_fields + [APIField("person")]
 
-    content_panels = BasePage.content_panels + [
-        SnippetChooserPanel('person')
-    ]
+    content_panels = BasePage.content_panels + [SnippetChooserPanel("person")]
 
     search_fields = BasePage.search_fields + [
-        index.RelatedFields('person', Person.search_fields)
+        index.RelatedFields("person", Person.search_fields)
     ]
